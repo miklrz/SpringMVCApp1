f2e7696e6c81590304729d3cca6cddf7f6258b62
miklrz <arz.mikhail@gmail.com>
2024-05-09 16:59:08 +0300

    42.pattern validation

--- src/main/java/ru/hxastur/springcourse/model/Person.java
+++ src/main/java/ru/hxastur/springcourse/model/Person.java
@@ -1,10 +1,7 @@
 package ru.hxastur.springcourse.model;
 
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 
 public class Person {
     private int id;
@@ -18,6 +15,10 @@ public class Person {
     @NotEmpty(message = "Email should not be empty")
     @Email(message = "Email should be valid")
     private String email;
+
+    // Страна, Город, Индекс (6 цифр)
+    @Pattern(regexp = "[A-Z]\\w+, [A-Z]\\w+, \\d{6}", message = "Your address should be in this format: Country, City, Postal Code (6 digits)")
+    private String address;
 
     public Person(){}
 
@@ -37,11 +38,12 @@ public class Person {
         this.email = email;
     }
 
-    public Person(int id, String name, int age, String email) {
+    public Person(int id, String name, int age, String email, String address) {
         this.id = id;
         this.name = name;
         this.age = age;
         this.email = email;
+        this.address = address;
     }
 
     public int getId() {
@@ -58,5 +60,13 @@ public class Person {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 }
